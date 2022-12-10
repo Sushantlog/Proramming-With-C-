14e7b6145725f47233db062345e5c4bc2966f0d9
sushantbpatil53@gmail.com <sushantbptil53@gmail.com>
2022-12-10 14:16:20 +0530

    first commit

---
+++ Intermediate DSA Stage 2/Stl/list.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main()
+{
+    list<int>l;
+
+    l.push_back(1);         //second
+    l.push_front(2);          //first
+
+    for(int i:l)
+    {
+        cout<<i<<endl;
+
+    }
+
+    l.erase(l.begin());         //first will be delete 
+    cout<<"after erese : "<<endl;
+    for(int i:l)
+    {
+        cout<<i<<endl;
+
+    }
+
+
+}
